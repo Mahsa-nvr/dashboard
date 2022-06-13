c9ad6af287b66f79fb7e3b86970c7fef37320f49
Mahsa-nvr <mahsaanvari2@gmail.com>
2022-06-13 22:08:00 +0430

    first setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+const App = () => {
+  return (
+    <h1 className="underline text-3x1">
+     app
+       
+    </h1>
+  );
+}
+
+export default App;
